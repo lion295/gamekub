c05d16c6d7f8a07c4cdca6e7f83bf10f356e656c
admin <admin@imac-admin-2>
2016-12-13 15:49:25 +0300

    add functionality kubik_go

--- ViewController2.swift
+++ ViewController2.swift
@@ -15,6 +15,27 @@ class ViewController2: UIViewController {
     
     let pers2i: UIImage = UIImage (named: "p2.png")!
     let pers2v: UIImageView = UIImageView ()
+    
+    @IBOutlet weak var kub_img: UIImageView!
+    
+    func kubik_go () -> Int{
+        let  k = Int(arc4random_uniform(6)+1)
+        let  kub_num: String = String(k) + ".png"
+        let ki: UIImage = UIImage(named: kub_num)!
+        kub_img.image = ki
+        view.addSubview(kub_img)
+        return k;
+        
+    
+    }
+    
+    
+    @IBAction func go(_ sender: UIButton) {
+        var a = kubik_go()
+    }
+    
+    
+    
     func pers1_view(){
         
         pers1v.image = pers1i
